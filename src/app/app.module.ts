5c25128374c56b1ac573198c2c066ac47082962d
Spenser Hardin <shardin@pillartechnology.com>
2018-04-15 07:58:32 -0500

    Implementing accordion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import {AppComponent} from './app.component';
 import {DogListComponent} from './dogs/dog-list/dog-list.component';
 import {DogItemComponent} from './dogs/dog-list/dog-item/dog-item.component';
 import {HeaderComponent} from './header/header.component';
-import {MatListModule} from '@angular/material';
+import {MatListModule, MatExpansionModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import {MatListModule} from '@angular/material';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     MatListModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dogs/dog-list/dog-list.component.ts
+++ src/app/dogs/dog-list/dog-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Dog} from '../../modules/dog.module';
 
 @Component({
@@ -7,10 +7,22 @@ import {Dog} from '../../modules/dog.module';
   styleUrls: ['./dog-list.component.css']
 })
 export class DogListComponent implements OnInit {
-  kaTea = new Dog('KaTea', 'Rough Collie', '', '', '', '');
-  london = new Dog('London', 'Rough Collie', '', '', '', '');
+  kaTea = new Dog('KaTea',
+    'Rough Collie',
+    'DN48001501',
+    '02/01/2016',
+    'CH Highcroft St. Germaine Wildfire',
+    'CH Highcroft Seaspray');
+  london = new Dog('London',
+    'Rough Collie',
+    'DN47009903',
+    '09/05/2016',
+    'CH Bannerstone Highcroft Vision',
+    'CH Highcroft Seaspray');
   dogList: Dog[] = [this.kaTea, this.london];
-  constructor() { }
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
